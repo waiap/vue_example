ad639d29673ebc1d5b8ba7385c9c4c169a5b3d62
David Francos <opensource@davidfrancos.net>
2019-11-21 16:39:19 +0100

    Fixed styles with prettier

--- src/pwall.js
+++ src/pwall.js
@@ -1,108 +1,122 @@
-const crypto = require('crypto');
-const request = require('request');
+const crypto = require("crypto");
+const request = require("request");
 
 class Pwall {
+  constructor(endpoint, key, secret, resource, mode = "sha256") {
+    this.endpoint = endpoint;
+    this.key = key;
+    this.secret = secret;
+    this.resource = resource;
+    this.mode = mode;
+  }
 
-    constructor(
-        endpoint,
-        key,
-        secret,
-        resource,
-        mode = 'sha256'
-    ) {
-        this.endpoint = endpoint;
-        this.key = key;
-        this.secret = secret;
-        this.resource = resource;
-        this.mode = mode;
-    }
+  action(
+    payload,
+    amount,
+    currency,
+    notify_url,
+    group_id = "0",
+    onResponse = () => {},
+    onPaymentSuccess = () => {},
+    onPaymentError = () => {}
+  ) {
+    return this.execute(
+      this.parseBody(payload, amount, currency, notify_url, group_id),
+      onResponse,
+      onPaymentSuccess,
+      onPaymentError
+    );
+  }
 
-    action(
+  execute(request_params, onResponse, onPaymentSuccess, onPaymentError) {
+    request.post(
+      {
+        url: this.endpoint,
+        headers: request_params.headers,
+        json: true,
+        body: request_params.body,
+        method: "POST"
+      },
+      function(error, httpResponse, res) {
+        if (
+          request_params.body["payload"]["action"] == "pwall.sale" &&
+          res["result"]["code"] == "0"
+        ) {
+          var method =
+            request_params.body["payload"]["method"] ||
+            request_params.body["payload"]["params"]["method"];
+          switch (method) {
+            case "fpay":
+              if (typeof res.result.payload.approval !== "undefined") {
+                onPaymentSuccess(res);
+              }
+              break;
+            default:
+              onPaymentSuccess(res);
+              break;
+          }
+        } else if (
+          request_params.body["payload"]["action"] == "sale" &&
+          res["result"]["code"] != "0"
+        ) {
+          onPaymentError(res);
+        }
+
+        onResponse(res);
+      }
+    );
+  }
+
+  parseBody(payload, amount, currency, notify_url, group_id) {
+    var body = {
+      key: this.key,
+      resource: this.resource,
+      nonce: process
+        .hrtime()
+        .toString()
+        .replace(",", ""),
+      mode: this.mode,
+      payload: this.parsePayload(
         payload,
         amount,
         currency,
         notify_url,
-        group_id='0',
-        onResponse = () => {},
-        onPaymentSuccess = () => {},
-        onPaymentError = () => {}) {
-        return this.execute(
-            this.parseBody(payload, amount, currency, notify_url, group_id),
-            onResponse,
-            onPaymentSuccess,
-            onPaymentError
-        );
-    }
+        group_id
+      )
+    };
 
-    execute(request_params, onResponse, onPaymentSuccess, onPaymentError) {
-        request.post({
-            url: this.endpoint,
-            headers: request_params.headers,
-            json: true,
-            body: request_params.body,
-            method: "POST"
-        }, function(error, httpResponse, res) {
+    return {
+      body: body,
+      headers: this.getHeaders(body)
+    };
+  }
 
-            if (request_params.body['payload']['action'] == 'pwall.sale' && res['result']['code'] == '0') {
-                var method = (request_params.body['payload']['method'] || request_params.body['payload']['params']['method'])
-                switch (method) {
-                    case 'fpay':
-                        if (typeof res.result.payload.approval !== 'undefined') {
-                            onPaymentSuccess(res);
-                        }
-                        break;
-                    default:
-                        onPaymentSuccess(res);
-                        break;
-                }
-            } else if (request_params.body['payload']['action'] == 'sale' && res['result']['code'] != '0') {
-                onPaymentError(res);
-            }
+  parsePayload(payload, amount, currency, notify_url, group_id) {
+    "params" in payload || (payload.params = {});
+    payload["params"]["group_id"] = group_id;
+    payload["params"]["amount"] = amount;
+    payload["params"]["currency"] = currency;
+    payload["params"]["token"] = Math.random()
+      .toString(36)
+      .substring(2, 15);
+    payload["params"]["order"] = Math.random()
+      .toString(36)
+      .substring(2, 15);
+    payload["params"]["notify"] = {
+      result: notify_url
+    };
+    return payload;
+  }
 
-            onResponse(res);
-        });
-    }
-
-    parseBody(payload, amount, currency, notify_url, group_id) {
-        var body = {
-            'key': this.key,
-            'resource': this.resource,
-            'nonce': process.hrtime().toString().replace(',', ''),
-            'mode': this.mode,
-            'payload': this.parsePayload(payload, amount, currency, notify_url, group_id)
-        }
-
-        return {
-            'body': body,
-            'headers': this.getHeaders(body)
-        };
-    }
-
-    parsePayload(payload, amount, currency, notify_url, group_id) {
-        'params' in payload || (payload.params = {});
-        payload['params']['group_id'] = group_id;
-        payload['params']['amount'] = amount;
-        payload['params']['currency'] = currency;
-        payload['params']['token'] = Math.random().toString(36).substring(2, 15);
-        payload['params']['order'] = Math.random().toString(36).substring(2, 15);
-        payload['params']['notify'] = {
-            'result': notify_url
-        };
-        return payload;
-    }
-
-    getHeaders(body) {
-        return {
-            'Content-Type': 'application/json',
-            'Content-Signature': crypto.createHmac(
-                this.mode, this.secret
-            ).update(
-                JSON.stringify(body)
-            ).digest(
-                'hex'
-            )
-        }
-    }
+  getHeaders(body) {
+    return {
+      "Content-Type": "application/json",
+      "Content-Signature": crypto
+        .createHmac(this.mode, this.secret)
+        .update(JSON.stringify(body))
+        .digest("hex")
+    };
+  }
 }
 
-module.exports = Pwall
+module.exports = Pwall;
--- src/server.js
+++ src/server.js
@@ -1,35 +1,42 @@
-var express = require('express');
-var pwall = require('./pwall');
-var cors = require('cors');
+var express = require("express");
+var pwall = require("./pwall");
+var cors = require("cors");
 var app = express();
 
 // Setup pwall class
-const _notify_url = 'http://92ef471a.ngrok.io/checkout' //Nombre del ngrok
+const _notify_url = "http://92ef471a.ngrok.io/checkout"; //Nombre del ngrok
 pwall = new pwall(
-  endpoint = 'https://sandbox.sipay.es/pwall/api/v1/actions',
-  key = 'sipay-test-team',
-  secret = 'api-secret',
-  resource = 'sipay-test-pwall',
-  mode = "sha256"
-)
+  (endpoint = "https://sandbox.sipay.es/pwall/api/v1/actions"),
+  (key = "sipay-test-team"),
+  (secret = "api-secret"),
+  (resource = "sipay-test-pwall"),
+  (mode = "sha256")
+);
 
 // Setup server
 app.use(cors());
 app.use(express.json());
 
-app.post('/actions/:amount', function (req, res) { // Don't use this on real world!!!!
+app.post("/actions/:amount", function(req, res) {
+  // Don't use this on real world!!!!
   pwall.action(
-    payload=req.body,
-    amount=parseInt(req.params.amount),
-    currency='EUR',
-    notify_url=_notify_url,
-    group_id='0',
-    onResponse=(data) => {res.json(data)},
-    onPaymentSuccess=(data) => {res.json(data)},
-    onPaymentError=(data) => {res.json(data)}
+    (payload = req.body),
+    (amount = parseInt(req.params.amount)),
+    (currency = "EUR"),
+    (notify_url = _notify_url),
+    (group_id = "0"),
+    (onResponse = data => {
+      res.json(data);
+    }),
+    (onPaymentSuccess = data => {
+      res.json(data);
+    }),
+    (onPaymentError = data => {
+      res.json(data);
+    })
   );
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000!");
 });
