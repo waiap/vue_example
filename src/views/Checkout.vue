fbc019bb6d88e233aeb11bec259dc748207bbc47
David Francos <opensource@davidfrancos.net>
2020-04-27 18:03:10 +0200

    Added IE11 compatibility

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,56 +1,63 @@
 <template>
-    <div id="pwall-container" style="display: none">
-        Payment container
-        <Pwall/>
-    </div>
+  <div id="pwall-container" style="display: none">
+    Payment container
+    <Pwall />
+  </div>
 </template>
 
 <script>
-import Pwall from '@/components/Pwall.vue'
+import Pwall from "@/components/Pwall.vue";
 export default {
-    name: 'checkout',
-    components: {
-        Pwall
-    },
-    props: {
-        model: {
-            type: Object,
-            default: null
-        }
-    },
-    data: () => ({
-        request_id: null,
-        method: null,
-        loaded: false
-    }),
-    mounted() {
-        /* eslint-disable */
-        /**
-         * if method and request_id are received them verify transaction with the event payment_wall_process_redirect
-         * and listen for the events  payment_wall_payment_ok or payment_wall_payment_ko
-         * */
-        if(this.$route.query.request_id && this.$route.query.method) {
-            document.getElementById('app').addEventListener("payment_wall_drawn", function(data) {
-                document.getElementById('app').addEventListener('payment_wall_payment_ok', ev => {
-                    alert('Payment Approved');
-                    console.log('payment_wall_payment_ok', ev);
-                });
-                document.getElementById('app').addEventListener('payment_wall_payment_ko', ev => {
-                    alert('Payment Denied');
-                    console.log('payment_wall_payment_ko', ev.detail);
-                });
-                document.dispatchEvent(new Event('payment_wall_process_redirect'));
-            });
-        } else {
-            document.addEventListener('payment_wall_load', function(){
-                console.log('payment_wall_load');
-                document.getElementById('app').addEventListener("payment_wall_loaded", ev => {
-                    console.log('payment_wall_loaded',ev.detail);
-                });
-            });
-        }
-        var container = document.getElementById('pwall-container');
-        container.style.display = '';
+  name: "checkout",
+  components: {
+    Pwall
+  },
+  props: {
+    model: {
+      type: Object,
+      default: null
     }
-}
+  },
+  data: () => ({
+    request_id: null,
+    method: null,
+    loaded: false
+  }),
+  mounted() {
+    /* eslint-disable */
+    /* If method and request_id are received them verify transaction with
+     * the event payment_wall_process_redirect
+     * and listen for the events  payment_wall_payment_ok or
+     * payment_wall_payment_ko
+     */
+    window.PaymentWall.start();
+    if (this.$route.query.request_id && this.$route.query.method) {
+      let placeholder = document.getElementById("app");
+      window.PaymentWall.listenTo("app", "drawn", function(data) {
+        window.PaymentWall.listenTo("app", "payment_ok", ev => {
+          alert("Payment Approved");
+          console.log("payment_wall_payment_ok", ev);
+        });
+        window.PaymentWall.listenTo("app", "payment_ko", ev => {
+          alert("Payment Denied");
+          console.log("payment_wall_payment_ko", ev.detail);
+        });
+        window.PaymentWall.dispatch("process_redirect", {
+          request_id: this.$route.query.request_id,
+          method: this.$route.query.method
+        });
+        document.dispatchEvent(new Event("payment_wall_process_redirect"));
+      });
+    } else {
+      window.PaymentWall.listenTo(placeholder, "load", function() {
+        console.log("payment_wall_load");
+        window.PaymentWall.listenTo(placeholder, "loaded", ev => {
+          console.log("payment_wall_loaded", ev.detail);
+        });
+      });
+    }
+    var container = document.getElementById("pwall-container");
+    container.style.display = "";
+  }
+};
 </script>
